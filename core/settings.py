d37811bdb3ccb77102f97eabdcd5621fb458fbae
Ramyar Arefi <arefi_ramyar99@yahoo.com>
2021-07-22 00:51:19 +0430

    as

--- core/settings.py
+++ core/settings.py
@@ -14,9 +14,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)@!kr48s5uca(&y+_ce2gvk(qbk7)e2747+htia!m$kpyv&uzl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
