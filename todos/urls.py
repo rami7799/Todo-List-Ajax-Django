de17260a0bf2b065d6803061d924ec308d439522
Ramyar Arefi <arefi_ramyar99@yahoo.com>
2021-07-21 18:29:26 +0430

    gfdsfg

--- todos/urls.py
+++ todos/urls.py
@@ -4,6 +4,7 @@ from .views import *
 
 urlpatterns = [
     path('' , home , name="home"),    
+    path("login" , login , name="login")
     path('todo-list' , todo_list , name="todo-list"),
     path("add-todo" , add_todo , name="add_todo"),    
     path("delete-todo" , delete_todo , name="delete-todo"),
--- todos/views.py
+++ todos/views.py
@@ -1,33 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Todos
 from django.http import JsonResponse , HttpResponse
 from django.template.loader import render_to_string 
+from django.contrib.auth import login , authenticate
+from django.contrib.auth.decorators import login_required
 
-
-
+@login_required(login_url="/login")
 def home(request):
     return render(request , "home.html")
 
+def login(request):
+    if request.method == "POST":
+        username = request.POST["usename"]
+        password = request.POST["password"]
+        user = authenticate(request , username=username , password=password)
+        if user is not None:
+            login(request , user)
+            return redirect("/")
+    return render(request , "login.html")
+
+
+@login_required(login_url="/login")
 def todo_list(request):
     todos = Todos.objects.order_by("-id")
     todos = render_to_string("ajax/todo-list.html" , {"todos" : todos})
     return JsonResponse({'data' : todos})
 
+
+@login_required(login_url="/login")
 def add_todo(request):
     if request.method == "POST":
         todo = request.POST.get("todo")
         print(todo)
         Todos.objects.create(title = todo)
         return HttpResponse("good")
-         
+
+
+@login_required(login_url="/login")
 def delete_todo(request):
     id = request.GET["id"]
     print(id)
     todo = Todos.objects.get(id=id)
     todo.delete()
     return HttpResponse("task deleted")
+    
 
-
+@login_required(login_url="/login")
 def update_todo(request):
     id = request.GET["id"]
     print(id)
