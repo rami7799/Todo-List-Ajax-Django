43a02450d1aeb657cbd02155577923a7c2784189
Ramyar Arefi <arefi_ramyar99@yahoo.com>
2021-07-22 00:59:11 +0430

    gfdsg

--- todos/models.py
+++ todos/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Todos(models.Model):
     title = models.CharField(max_length=150)
     text = models.CharField(max_length=150)
     completed = models.BooleanField(default=False)
+    user = models.ForeignKey(User , on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return self.title
